12c3c62c8f490d31f7129aa7c17f028d964bd29e
Gence0225 <hisakawa0225@gmail.com>
2026-07-24 12:47:02 +0900

    sw.js の削除

--- sw.js
+++
@@ -1,5 +0,0 @@
-const C='v2calc-v1';
-const FILES=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request)))});
