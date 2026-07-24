e6887fec94f73b97fe13519ed817bfe01de3e4e8
Gence0225 <hisakawa0225@gmail.com>
2026-07-24 12:50:31 +0900

    sw.js の作成

---
+++ sw.js
@@ -0,0 +1,5 @@
+const C='v2calc-v8';
+const FILES=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request)))});
