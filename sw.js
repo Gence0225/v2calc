93176eb0bb40ca7fc76d24b5155cf9bb6be6b118
Gence0225 <hisakawa0225@gmail.com>
2026-08-08 07:57:45 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='v2calc-v11';
+const C='v2calc-jo1';
 const FILES=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim()});
