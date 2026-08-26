4f5e47cf637531226ecbf32bb2223a714c6e9129
Gence0225 <hisakawa0225@gmail.com>
2026-08-26 12:30:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='yatakagami-jo7';
+const C='yatakagami-jo8';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>k!==C&&caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
